dde340f4c58bb1943eee7d15cfb0c9c4a50a62ab
Bia-z <beatriz-cmt@hotmail.com>
2026-08-12 18:00:09 -0300

    organizando e adicionando funcionalidades

---
+++ Consumidor.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+// ATRIBUTOS
+public class Consumidor {
+    private int codigo;
+    private double consumo;
+    private int baixaRenda;
+    private int bandeira;
+
+
+    // CONSTRUTOR
+    public Consumidor(int codigo, double consumo, int baixaRenda, int bandeira) {
+        this.codigo = codigo;
+        this.consumo = consumo;
+        this.baixaRenda = baixaRenda;
+        this.bandeira = bandeira;
+    }
+
+    // GETTERS
+    public int getCodigo() {return this.codigo;}
+    public double getConsumo() {return this.consumo;}
+    public int getBaixaRenda() {return this.baixaRenda;}
+    public int getBandeira() {return this.bandeira;}
+
+    // SETTERS
+    public void setCodigo(int codigo) {this.codigo = codigo;}
+    public void setConsumo(double consumo) {this.consumo = consumo;}
+    public void setBaixaRenda(int baixaRenda) {this.baixaRenda = baixaRenda;}
+    public void setBandeira(int bandeira){this.bandeira = bandeira;}
+
+    // MÉTODOS
+    public double calcularConta()
+    {
+        double valorFinal;
+        if (this.consumo <= 100) {valorFinal = (this.consumo * 0.6);}
+
+        else if (this.consumo <=200) {valorFinal = (100 * 0.6) + ((this.consumo - 100) * 0.75);}
+
+        else {valorFinal = (100 * 0.75) + (100 * 0.6) + ((this.consumo - 200) * 0.9);}
+
+        if (this.bandeira == 2) {valorFinal = valorFinal + (this.consumo*0.05);}
+
+        else if (this.bandeira == 3) {valorFinal = valorFinal + (this.consumo*0.1) ;}
+
+        if (this.baixaRenda == 1) {
+            if (this.consumo <= 150) {valorFinal = valorFinal - (0.1 * valorFinal);}
+        }
+
+        return valorFinal;
+    }
+
+}
--- Main.java
+++ Main.java
@@ -8,56 +8,20 @@ public class Main
     public static void main(String[] args) throws IOException
     {
         System.out.println("Sistema de cálculo de contas de energia.");
-
-        System.out.println("Quantos consumidores serão processados?");
         BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-        int qntdd = Integer.parseInt(teclado.readLine());
 
-        ArrayList<Integer> codigos = new ArrayList<>();
-        ArrayList<Double> consumo = new ArrayList<>();
-        ArrayList<Integer> renda = new ArrayList<>();
-        ArrayList<Integer> bandeira = new ArrayList<>();
+        Cadastro sistema = new Cadastro();
 
-        for (int i = 0; i<qntdd; i++){
-        System.out.println("Por favor digite o código numérico do " + (i+1) + " consumidor: ");
-        codigos.add(Integer.parseInt(teclado.readLine()));
-
-        System.out.println("Digite o consumo mensal do " + (i+1) + " cliente em kWh: ");
-        consumo.add(Double.parseDouble(teclado.readLine()));
-
-        System.out.println("O cliente participa do programa de baixa renda?");
-        System.out.println("1 - Sim, participa.");
-        System.out.println("0 - Não, não participa.");
-        renda.add(Integer.parseInt(teclado.readLine()));
-
-        System.out.println("Qual a cor da bandeira da conta?");
-        System.out.println("1 - Verde");
-        System.out.println("2- Amarela");
-        System.out.println("3- Vermelha");
-        bandeira.add(Integer.parseInt(teclado.readLine()));
-
-        }
-
-        System.out.println("MENU");
-        System.out.println("1. Cadastrar novo consumidor");
-        System.out.println("2. Consultar dados de um consumidor específico.");
-        System.out.println("3. Apagar um consumidor.");
-        System.out.println("4. Atualizar dados de um consumidor.");
-        System.out.println("5. Relatórios.");
-        System.out.println("6. Listar consumidores.");
-        System.out.println("7. Sair do programga.");
-        System.out.println("Escolha uma opção: ");
         int opcao = 0;
-
         while (opcao != 7) {
             menu();
             opcao = Integer.parseInt(teclado.readLine());
 
             if (opcao == 1) {
-                System.out.println("Opção não implementada.");
+                sistema.cadastrarConsumidor(teclado);
             }
             else if (opcao == 2) {
-                consultarConsumidor();
+                sistema.consultarConsumidor(teclado);
             }
             else if (opcao == 3) {
                 System.out.println("Opção não implementada.");
@@ -69,7 +33,7 @@ public class Main
                 System.out.println("Opção não implementada.");
             }
             else if (opcao == 6) {
-                consultarConsumidor();
+                sistema.consultarConsumidor(teclado);
             }
             else if (opcao == 7) {
                 System.out.println("Encerrando o programa...");
@@ -79,70 +43,13 @@ public class Main
     }
 
     public static void menu() {
-        System.out.println("MENU");
-        System.out.println("1. Cadastrar novo consumidor");
-        System.out.println("2. Consultar dados de um consumidor específico.");
-        System.out.println("3. Apagar um consumidor.");
-        System.out.println("4. Atualizar dados de um consumidor.");
-        System.out.println("5. Relatórios.");
-        System.out.println("6. Listar consumidores.");
-        System.out.println("7. Sair do programga.");
-    }
-
-
-    public static void consultarConsumidor(BufferedReader teclado, ArrayList<Integer> codigos, ArrayList<Double> consumo, ArrayList<Integer> renda, ArrayList<Integer> bandeira)  throws IOException
-    {
-        System.out.println("Digite o código numérico do consumidor: ");
-        int codBuscado = Integer.parseInt(teclado.readLine());
-        for (int i =  0; i<codigos.size(); i++) {
-            if (codigos.get(i) == codBuscado) {
-                System.out.println("Código do consumidor: " + codigos.get(i));
-                System.out.println("Consumo em kWh: " + consumo.get(i));
-                System.out.println("Bandeira: " + bandeira.get(i));
-                if (renda.get(i) == 1) {
-                System.out.println("Baixa renda: Sim"); }
-                else {
-                    System.out.println("Baixa renda: Não"); }
-                double valorFinal = calcularConta(consumo.get(i), bandeira.get(i), renda.get(i));
-                System.out.println("Valor final da conta: " + valorFinal);
-            }
-            else {
-                i++;
-            }
-            //PAREI AQUI
-        }
-
-    }
-
-    public static
-
-
-    public static double calcularConta(double consumo, int bandeira, int baixaRenda)
-    {
-        double valorFinal;
-        if (consumo <= 100) {
-            valorFinal = (consumo * 0.6);
-        }
-        else if (consumo <=200) {
-            valorFinal = (100 * 0.6) + ((consumo - 100) * 0.75);
-        }
-        else {
-            valorFinal = (100 * 0.75) + (100 * 0.6) + ((consumo - 200) * 0.9);
-        }
-
-        if (bandeira == 2) {
-            valorFinal = valorFinal + (consumo*0.05);
-        }
-        else if (bandeira == 3) {
-            valorFinal = valorFinal + (consumo*0.1) ;
-        }
-
-        if (baixaRenda == 1) {
-            if (consumo <= 150) {
-                valorFinal = valorFinal - (0.1 * valorFinal);
-            }
-        }
-
-        return valorFinal;
+        System.out.printf("MENU\n");
+        System.out.printf("1. Cadastrar novo consumidor.\n");
+        System.out.printf("2. Consultar dados de um consumidor específico.\n");
+        System.out.printf("3. Apagar um consumidor.\n");
+        System.out.printf("4. Atualizar dados de um consumidor.\n");
+        System.out.printf("5. Relatórios.\n");
+        System.out.printf("6. Listar consumidores.\n");
+        System.out.printf("7. Sair do programga.\n");
     }
 }
